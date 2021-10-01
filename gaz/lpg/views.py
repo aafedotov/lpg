e0dac0f5452ed1a2d808b50d5a405719abaf4648
Andrey Fedotov <mr.a.fedotov@yandex.ru>
2021-10-01 08:45:56 -0400

    bdtxt

--- gaz/lpg/views.py
+++ gaz/lpg/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+import datetime
 
 def lpg_view(request):
     template = 'lpg/lpg.html'
     status = ''
+    context = {'status' : '',
+            'pricelpg' : '',
+            'litres' : '',
+            'mileage' : '',
+            'price95' : ''}
     if request.method == 'POST':
         status = 'Данные успешно переданы на сервер!'
-        
-    context = {'status': status}
+        now = datetime.datetime.now()
+        date = now.strftime("%d.%m.%Y")
+        pricelpg = request.POST['pricelpg']
+        litres = request.POST['litres']
+        mileage = request.POST['mileage']
+        price95 = request.POST['price95']
+        f = open('bd.txt', 'a')
+        f.write(f'{date}  {pricelpg}  {litres}  {mileage}  {price95} \n')
+        f.close()
+        context = {'status' : status}
+
     return render(request, template, context)
