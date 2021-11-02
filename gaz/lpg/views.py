25bb32265ff4b5a72d287df8738c17721803d795
Федотов Андрей Анатольевич <FedotovAAn@gaz.ru>
2021-11-02 13:21:43 +0300

    parsing_price

--- gaz/lpg/views.py
+++ gaz/lpg/views.py
@@ -3,13 +3,31 @@ import datetime
 from .models import Lpg
 from django.db.models import Avg
 import json
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_benz_price():
+    """Парсим текущую цену бензина."""
+    url = 'https://fuelprice.ru/azs16561'
+    response = requests.get(url, verify=False)
+    soup = BeautifulSoup(response.text, 'lxml')
+    prices = soup.find_all('span', class_='text-success font-weight-bold')
+    dates = soup.find_all('small', class_='text-')
+    price = {'price': prices[1].text, 'date': dates[0].text}
+    return price
+
 
 def lpg_view(request):
     if request.user.username != 'faa':
         return redirect('/auth/login/')
     template = 'lpg/lpg.html'
-    context = {'status' : '',
-            'is_lpg':True,}
+    price = get_benz_price()
+    context = {'status': '',
+               'is_lpg': True,
+               'price': price['price'],
+               'date': price['date'],
+               }
     if request.method == 'POST':
         status = 'Данные успешно переданы на сервер!'
         now = datetime.datetime.now()
@@ -20,18 +38,19 @@ def lpg_view(request):
         last_lpg = Lpg.objects.all().order_by('-date').first()
         f = Lpg()
         f.date = now
-        f.price = round(float(pricelpg), 2) 
+        f.price = round(float(pricelpg), 2)
         f.volume = round(float(litres), 2)
         f.benz_price = round(float(price95), 2)
-        f.cost = round(float(pricelpg)*float(litres), 2)
+        f.cost = round(float(pricelpg) * float(litres), 2)
         f.mileage = round(float(mileage), 2)
         f.mileage_total = round((last_lpg.mileage_total + float(mileage)), 2)
-        f.consump = round((last_lpg.volume/float(mileage)*100), 2)
-        f.saving = round((float(mileage)/100*11.5*last_lpg.benz_price-last_lpg.cost), 2)
+        f.consump = round((last_lpg.volume / float(mileage) * 100), 2)
+        f.saving = round((float(
+            mileage) / 100 * 11.5 * last_lpg.benz_price - last_lpg.cost), 2)
         f.save()
-        context = {'status' : status,
-                   'is_lpg':True,
-        }
+        context = {'status': status,
+                   'is_lpg': True,
+                   }
     return render(request, template, context)
 
 
@@ -75,15 +94,15 @@ def lpg_summary(request):
     total_days = total_days.days
     total_mileage = int(first_lpg.mileage_total)
     context = {
-            'total_saving': total_saving,
-            'total_volume': total_volume,
-            'total_cost': total_cost,
-            'total_consump': total_consump,
-            'total_days': total_days,
-            'total_mileage': total_mileage,
-            'lpg': lpgs,
-            'is_lpg':True,
-            'chart_data': chart_data,
-            'chart_mileage': chart_mileage,
-            }
+        'total_saving': total_saving,
+        'total_volume': total_volume,
+        'total_cost': total_cost,
+        'total_consump': total_consump,
+        'total_days': total_days,
+        'total_mileage': total_mileage,
+        'lpg': lpgs,
+        'is_lpg': True,
+        'chart_data': chart_data,
+        'chart_mileage': chart_mileage,
+    }
     return render(request, template, context)
