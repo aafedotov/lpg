ef2afe42dfe3bc645770e76c5408fd6e6d3d4531
Andrey Fedotov <fedotov_aa@outlook.com>
2022-07-11 19:33:54 +0300

    add_web3_storage_api

--- gaz/meta/views.py
+++ gaz/meta/views.py
@@ -1,10 +1,17 @@
 import base64
 import json
+import datetime as dt
+import requests
 
+from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
-
+from pathlib import Path
 
 from .forms import MemoryForm
+
+BASE_DIR = Path(__file__).parent
+TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDliZTJFZGY1NTBGMDEwZDQ1MzYxMWQwNDMxN0Q3MTU1RUU2NjQ2MTQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTc1MzU5MzU0OTcsIm5hbWUiOiJtZXRhbWVtb3J5In0.h3xx_IsIF-VqnOP0H0hVCoJ-kdN8m0MONOkxKRrSpdg'
+
 
 
 def meta_success(request):
@@ -32,8 +39,24 @@ def index(request):
         for char in obj_json:
             hex_hash += hex(ord(char))[2:]
         obj.hex_hash = hex_hash
+        graves_dir = BASE_DIR / 'graves'
+        graves_dir.mkdir(exist_ok=True)
+        now = dt.datetime.now()
+        now_formatted = now.strftime('%Y-%m-%d_%H-%M-%S')
+        file_name = f'{obj_dict["name"]}_{now_formatted}.txt'
+        with open(graves_dir / file_name, "w") as text_file:
+            text_file.write(hex_hash)
         obj.save()
+        multipart_form_data = {
+            'file': (file_name, open(graves_dir / file_name, 'rb'), 'text/plain')
+        }
 
+        headers = {'Authorization': f'Bearer {TOKEN}'}
+
+        response = requests.post('https://api.web3.storage/upload',
+                                 files=multipart_form_data, headers=headers,
+                                 verify=False)
+        messages.add_message(request, messages.INFO, response.text)
         return redirect(reverse('meta:success'))
     template = 'meta/index.html'
     context = {'form': form}
