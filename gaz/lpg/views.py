7fa3f215d44c496e91344f48a8cef95b8b206335
Андрей Федотов <mr.a.fedotov@yandex.ru>
2023-09-28 08:15:22 +0300

    fix_benz_price_parser_moscow

--- gaz/lpg/views.py
+++ gaz/lpg/views.py
@@ -8,12 +8,12 @@ from bs4 import BeautifulSoup
 
 def get_benz_price():
     """Парсим текущую цену бензина."""
-    url = 'https://fuelprice.ru/azs16708'
+    url = 'https://fuelprice.ru/azs10608'
     response = requests.get(url, verify=False)
     soup = BeautifulSoup(response.text, 'lxml')
     prices = soup.find_all('span', class_='text-success font-weight-bold')
     dates = soup.find_all('small', class_='text-')
-    price = {'price': prices[2].text, 'date': dates[0].text}
+    price = {'price': prices[4].text, 'date': dates[4].text}
     return price
 
 
